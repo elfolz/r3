1ed354bfabce95397bbe569d6c9b4328db290037
Luiz <lhvalentim@outlook.coom>
2024-04-10 10:27:40 -0300

    query params

--- js/main.js
+++ js/main.js
@@ -7,6 +7,14 @@ if (location.protocol.startsWith('https')) {
 }
 
 function init() {
+	const params = new URLSearchParams(location.search)
+	const aVal = params.get('a')
+	if (aVal) document.querySelector('#valor1').value = aVal
+	const bVal = params.get('b')
+	if (bVal) document.querySelector('#valor2').value = bVal
+	const cVal = params.get('c')
+	if (cVal) document.querySelector('#valor3').value = cVal
+	if (aVal && bVal && cVal) refresh()
 	document.querySelectorAll('input').forEach(el => {
 		if (el.classList.contains('masked')) return
 		IMask(el, {
@@ -19,15 +27,17 @@ function init() {
 			radix: '.',
 			mapToRadix: ['.', ',']
 		})
-		el.oninput = () => {
-			const v1 = parseFloat(document.querySelector('#valor1')?.value?.replace(',', '.'))
-			const v2 = parseFloat(document.querySelector('#valor2')?.value?.replace(',', '.'))
-			const v3 = parseFloat(document.querySelector('#valor3')?.value?.replace(',', '.'))
-			if (!v1 || !v2 || !v3) return
-			document.querySelector('#valor4').innerHTML = (v2 * v3 / v1).toFixed(2)
-		}
+		el.oninput = () => refresh()
 		el.classList.add('masked')
 	})
+}
+
+function refresh() {
+	const v1 = parseFloat(document.querySelector('#valor1')?.value?.replace(',', '.'))
+	const v2 = parseFloat(document.querySelector('#valor2')?.value?.replace(',', '.'))
+	const v3 = parseFloat(document.querySelector('#valor3')?.value?.replace(',', '.'))
+	if (!v1 || !v2 || !v3) return
+	document.querySelector('#valor4').innerHTML = (v2 * v3 / v1).toFixed(2)
 }
 
 document.onreadystatechange = () => {
